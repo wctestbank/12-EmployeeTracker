e722ddd698e939aa291be6e26e7b713fcaa7341d
wctestbank <wc.businessmail@gmail.com>
2022-05-25 08:39:32 -0400

    added add functions

--- utils/sql.js
+++ utils/sql.js
@@ -27,8 +27,33 @@ function displaysEmployees() {
     });
 }
 
-function addDepartment() {
-    const sql = `INSERT INTO `;
+function addDepartment(name) {
+    const sql = `INSERT INTO department (name) VALUES (?)`;
+    const params = [name];
+
+    db.query(sql, params, (err, result) => {
+        console.log(`Added ${name} to departments`);
+    });
 }
 
-module.exports = { displayDepartments, displaysRoles, displaysEmployees };
+function addRole(title, salary, department_id) {
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`;
+    const params = [title, salary, department_id];
+
+    db.query(sql, params, (err, result) => {
+        console.log(`Added ${title} to Roles`);
+    });
+}
+
+function addEmployee(first_name, last_name, role_id, manager_id) {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+    const params = [first_name, last_name, role_id, manager_id];
+
+    db.query(sql, params, (err, result) => {
+        console.log(`Added ${first_name} ${last_name} to Employees`);
+    });
+}
+
+
+module.exports =
+    { displayDepartments, displaysRoles, displaysEmployees, addDepartment, addRole, addEmployee };
